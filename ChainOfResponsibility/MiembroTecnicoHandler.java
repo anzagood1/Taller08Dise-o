f58b8968ce78a573d78d37e43a686f6912fc3901
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-07-16 10:15:53 -0500

    Create MiembroTecnicoHandler.java

---
+++ ChainOfResponsibility/MiembroTecnicoHandler.java
@@ -0,0 +1,8 @@
+public class MiembroTecnicoHandler extends HandlerBase{
+
+    //Metodo para manejar parte de la la solicitud
+    @Override
+    public void handle(Request request){
+        //Se verifica el tipo de fallo
+    }
+}
