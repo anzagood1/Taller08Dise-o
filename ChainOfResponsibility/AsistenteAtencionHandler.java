4d860e3cd5c58350eb493299995bc46dce9f9ac7
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-07-16 10:15:35 -0500

    Create AsistenteAtencionHandler.java

---
+++ ChainOfResponsibility/AsistenteAtencionHandler.java
@@ -0,0 +1,8 @@
+public class AsistenteAtencionHandler extends HandlerBase{
+
+    //Metodo para manejar parte de la la solicitud
+    @Override
+    public void handle(Request request){
+        //Se verifica el periodo de garantia
+    }
+}
