47ad3329ded08be8ac8d48de8089ac2d8309e13a
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-07-16 10:16:38 -0500

    Create GerenteTiendaHandler.java

---
+++ ChainOfResponsibility/GerenteTiendaHandler.java
@@ -0,0 +1,8 @@
+public class GerenteTiendaHandler extends HandlerBase{
+
+    //Metodo para manejar parte de la la solicitud
+    @Override
+    public void handle(Request request){
+        //Se aprueba o rechaza el pedido de cambio
+    }
+}
