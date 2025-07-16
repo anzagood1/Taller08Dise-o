277e3abb86333ea25c0b84cf31135d885c279c7b
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-07-16 10:16:22 -0500

    Create JefeInventarioHandler.java

---
+++ ChainOfResponsibility/JefeInventarioHandler.java
@@ -0,0 +1,8 @@
+public class JefeInventarioHandler extends HandlerBase{
+
+    //Metodo para manejar parte de la la solicitud
+    @Override
+    public void handle(Request request){
+        //Se verifica existencias en el inventario
+    }
+}
